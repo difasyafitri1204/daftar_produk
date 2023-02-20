f9f5d4e300423bdcb9a9b6457a69642b0025b2e1
difasyafitri <dfsyr08@gmailcom>
2023-02-20 12:00:40 +0700

    java

--- aplikasi-daftar-produk.js
+++ aplikasi-daftar-produk.js
@@ -3,6 +3,14 @@ function simpanProduk(form) {
     aplikasiDaftarProduk.inputProduk(form);
     aplikasiDaftarProduk.menampilkanDaftarProduk();
 }
+const dbDaftarProduk = {
+    save(daftarProduk2) {
+        localStorage.setItem('daftarProduk', JSON.stringify(daftarProduk2));
+    },
+    get() {
+        return JSON.parse(localStorage.getItem('daftarProduk'));
+    }
+}
 const aplikasiDaftarProduk = {
     produk: {
         index: -1,
@@ -37,10 +45,12 @@ const aplikasiDaftarProduk = {
         }
         
         if(this.produk.index == -1) {
+            this.daftarProduk = this.daftarProduk || [];
             this.daftarProduk.push(copy(this.produk));
         } else {
             this.daftarProduk[this.produk.index] = copy(this.produk)
         }
+        dbDaftarProduk.save(this.daftarProduk);
         this.resetFormProduk(form);
     },
     
@@ -60,17 +70,24 @@ const aplikasiDaftarProduk = {
         document.getElementById('btn-save-produk').innerHTML = 'Simpan';
     },
     menampilkanDaftarProduk: function () {
+        this.daftarProduk = dbDaftarProduk.get();
         const componentDaftarProduk = document.getElementById('daftar-produk');
         componentDaftarProduk.innerHTML = '';
-        this.daftarProduk.forEach((produk, index) => {
-            componentDaftarProduk.innerHTML +=  `<h4>${produk.produk} <br> ${produk.harga} <br> stok: ${produk.stok} <br> <img src="${produk.gambar}" width= "100px"><button 
-            class="btn btn-accent btn-active" onclick="aplikasiDaftarProduk.editProduk(${index})">Edit</button><button 
-            class="btn btn-accent btn-active" onclick="aplikasiDaftarProduk.hapusProduk(${index})"> Hapus </button></h4>`;
-        });
+        if (this.daftarProduk === null) {
+            console.log ('tidak ada data');
+        } else {
+            this.daftarProduk.forEach((produk, index) => {
+                componentDaftarProduk.innerHTML +=  `<h4><div class="flex justify-center gap-5"> <div class="card-actions justify-end">${produk.produk} <br> ${produk.harga} <br> stok: ${produk.stok} <br> <img src="${produk.gambar}" width= "100px"><button 
+                class="btn btn-accent btn-active" onclick="aplikasiDaftarProduk.editProduk(${index})">Edit</button><button 
+                class="btn btn-accent btn-active" onclick="aplikasiDaftarProduk.hapusProduk(${index})"> Hapus </button></div></div></h4>`;
+            });
+        }
+        
     },
     hapusProduk: function (index) {
         if(confirm('Apakah anda yakin ingin menghaapus data ini?')) {
             this.daftarProduk.splice(index, 1);
+            dbDaftarProduk.save(this.daftarProduk);
             this.menampilkanDaftarProduk();
         }
     },
@@ -91,3 +108,5 @@ const aplikasiDaftarProduk = {
 function copy(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
+
+aplikasiDaftarProduk.menampilkanDaftarProduk();
